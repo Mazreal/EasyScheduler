b0533f1b3ee16692b633933b5d6feafcf20669dd
Matthew <matthewespiritu8@gmail.com>
2021-11-10 18:05:12 -0700

    test

--- src/main/java/com/mycompany/easyscheduler/models/Employee.java
+++ src/main/java/com/mycompany/easyscheduler/models/Employee.java
@@ -18,5 +18,62 @@ public class Employee {
     private String phone;
     private Date birthday;
     private String username;
-    private String password;      
+    private String password; 
+    
+    public int getEmployee_ID() {
+        return employee_ID;
+    }
+
+    public void setEmployee_ID(int employee_ID) {
+        this.employee_ID = employee_ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+         
 }
